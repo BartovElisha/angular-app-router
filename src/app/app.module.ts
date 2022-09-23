2795188a31f5855671dde9270b29da3dad1870d0
ElishaBartov <elisha.bartov@emerson.com>
2022-09-23 21:40:30 +0300

    Initial commit, added components and tab menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { UsersComponent } from './component/users/users.component';
+import { SettingsComponent } from './component/settings/settings.component';
+import { TabsComponent } from './component/tabs/tabs.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    UsersComponent,
+    SettingsComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
